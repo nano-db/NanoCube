cb874d4557be65a590de06a29723c1ccb7ba3029
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-12-11 16:13:32 +0800

    Add docstring

--- libs/timeserietable.py
+++ libs/timeserietable.py
@@ -11,6 +11,11 @@ class TimeSerieTable(object):
         self.table = []
 
     def insert(self, time):
+        """ Insert an event into the table
+
+        :param time: datetime time to insert
+        :return:
+        """
         if len(self.table) == 0:
             self.start = time
             new_bin = {
@@ -21,7 +26,7 @@ class TimeSerieTable(object):
 
         else:
             bin_num = self._get_bin_number(time)
-            self._expand_table(time, bin_num)
+            self._expand_table(bin_num)
 
             if bin_num < 0:
                 bin_num = 0
@@ -32,11 +37,20 @@ class TimeSerieTable(object):
             self._update_following_bins(bin_num)
 
     def _get_bin_number(self, time):
+        """Return the bin index for the specified type
+
+        :param time: datetime
+        :rtype : int
+        """
         delta = (time - self.start).total_seconds() / self.bin_size
         return int(floor(delta))
 
-    def _expand_table(self, time, bin_num):
-        if bin_num >= 0 and bin_num < len(self.table):
+    def _expand_table(self, bin_num):
+        """Expand the current time table to accept new insertion
+
+        :param bin_num: int expected index
+        """
+        if 0 <= bin_num < len(self.table):
             return
 
         if bin_num < 0:
@@ -58,11 +72,21 @@ class TimeSerieTable(object):
                 self.table.append(new_bin)
 
     def _update_following_bins(self, bin_num):
+        """Update the table starting at the bin_num index
+
+        :param bin_num: int
+        """
         for i in range(bin_num + 1, len(self.table)):
             new_sum = self.table[i - 1]['sum'] + self.table[i]['count']
             self.table[i]['sum'] = new_sum
 
     def query(self, begin, end):
+        """Calculate the number of event during the specified timeframe
+
+        :param begin:  datetime beginning of the timeframe
+        :param end: datetime end of the timeframe
+        :return: int
+        """
         begin_bin = self._get_bin_number(begin)
         if begin_bin < 0:
             begin_bin = 0
@@ -74,6 +98,10 @@ class TimeSerieTable(object):
         return self.table[end_bin]['sum'] - self.table[begin_bin]['sum']
 
     def copy(self):
+        """Return a shallow copy of the instance
+
+        :return: TimeSerieTable
+        """
         table_copy = copy.copy(self)
         table_copy.table = []
         for elem in self.table:
