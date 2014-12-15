a53616fee383ac31692e6d0340b015ec7f386087
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-12-15 09:02:23 +0800

    Query but still need to fix the geolocation

--- libs/querybuilder.py
+++ libs/querybuilder.py
@@ -6,14 +6,19 @@ class QueryBuilder(object):
         self.precision = [0 for _ in range(cube.get_dimension())]
         self.timeframe = [None, None]
 
+        if kwargs.has_key('debug'):
+            self._init_debug()
+        else:
+            self.debug = False
+
     def find(self, dimension_name, dive):
-        if dimension_name not in self.cube.dimension and dimension_name != "Location":
+        if dimension_name not in self.cube.dimensions and dimension_name != "Location":
             raise Exception("Dimension '{0}' not found.".format(dimension_name))
 
         if dimension_name == "Location":
             path_index = 0
         else:
-            path_index = self.cube.dimension.index(dimension_name) + 1
+            path_index = self.cube.dimensions.index(dimension_name) + 1
 
         if len(self.path[path_index]) != 0:
             raise Exception("Try to override dimension: {0}".format(dimension_name))
@@ -22,7 +27,7 @@ class QueryBuilder(object):
             new_path = dive[0]
             new_precision = dive[1]
         else:
-            new_path = dive
+            new_path = [dive]
             new_precision = 0
 
         self.path[path_index] = new_path
@@ -44,23 +49,40 @@ class QueryBuilder(object):
 
         remaining_levels = self.cube.get_dimension()
         for level_path in path:
+            if self.debug:
+                visited_links = []
+
             for key in level_path:
                 if node.has_shared_content:
                     break
                 node = node.get_child(key)
                 if node is None:
                     return node
+                elif self.debug:
+                    visited_links.append(key)
+
             remaining_levels -= 1
             node = node.content
+            if self.debug:
+                self.trace.append(visited_links)
 
         for i in range(remaining_levels):
             node = node.content
+            if self.debug:
+                self.trace.append([])
 
         return node
 
-    def execute(self):
+    def execute(self, **kwargs):
+        if kwargs.has_key('debug'):
+            self._init_debug()
+
         table = self._retrieve_timeserietable(self.path)
         if table is None:
             return 0
         else:
             return table.query(self.timeframe[0], self.timeframe[1])
+
+    def _init_debug(self):
+        self.debug = True
+        self.trace = []
--- test/test_querybuilder.py
+++ test/test_querybuilder.py
@@ -2,10 +2,35 @@ from nose.tools import assert_equals
 from helpers import mock_cube
 from libs.querybuilder import QueryBuilder
 
+
 class TestQueryBuilder:
     def test_count(self):
         cube = mock_cube()
         count = QueryBuilder(cube).execute()
         assert_equals(count, 5)
 
+    def test_enable_trace_in_constructor(self):
+        cube = mock_cube()
+        q = QueryBuilder(cube, debug=True)
+        q.execute()
+        assert(hasattr(q, 'trace'))
 
+    def test_enable_trace_in_exec(self):
+        cube = mock_cube()
+        q = QueryBuilder(cube)
+        q.execute(debug=True)
+        assert(hasattr(q, 'trace'))
+
+    def test_trace_count(self):
+        cube = mock_cube()
+        q = QueryBuilder(cube)
+        q.execute(debug=True)
+        assert_equals(q.trace, [[], []])
+
+    def test_trace_full(self):
+        cube = mock_cube()
+        q = QueryBuilder(cube)\
+            .find("Location", (["1,0", "10,00"], 0))\
+            .find("Devise", cube.dim_mapping['Devise']['iPhone'])
+        q.execute(debug=True)
+        assert_equals(q.trace, [["1,0"], ["1"]])
