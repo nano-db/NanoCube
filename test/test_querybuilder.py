ea805ed8a88cf374080da4b352015c751d490d64
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-12-15 09:21:10 +0800

    Fix sample and implement more complex tests

--- test/test_querybuilder.py
+++ test/test_querybuilder.py
@@ -30,7 +30,23 @@ class TestQueryBuilder:
     def test_trace_full(self):
         cube = mock_cube()
         q = QueryBuilder(cube)\
-            .find("Location", (["1,0", "10,00"], 0))\
+            .find("Location", (["1,0", "11,01"], 0))\
             .find("Devise", cube.dim_mapping['Devise']['iPhone'])
         q.execute(debug=True)
-        assert_equals(q.trace, [["1,0"], ["1"]])
+        assert_equals(q.trace, [["1,0", "11,01"], []])
+
+    def test_without_restriction(self):
+        cube = mock_cube()
+        q = QueryBuilder(cube)\
+            .find("Location", (["1,0"], 0))
+        count = q.execute(debug=True)
+        assert_equals(q.trace, [["1,0"], []])
+        assert_equals(count, 2)
+
+    def test_to_content(self):
+        cube = mock_cube()
+        q = QueryBuilder(cube)\
+            .find("Devise", cube.dim_mapping['Devise']['iPhone'])
+        count = q.execute(debug=True)
+        assert_equals(q.trace, [[], ["1"]])
+        assert_equals(count, 3)
