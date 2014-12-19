b927c2a94d8bc7fecb0a3322ca77e03401817e90
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-12-19 14:47:34 +0800

    Add formating files

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ def reqs():
     return [str(ir.req) for ir in install_reqs]
 
 setup(
-    name="NanoDB",
-    version="0.0.1",
+    name="nanodb",
+    version="0.1",
     license="Apache License 2.0",
     author="Pierre-Marie Dartus",
     author_email="dartus.pierremarie@gmail.com",
     url="https://github.com/pmdartus/NanoCube",
     description="In memory database for geolocated and temporal data",
+    download_url="https://github.com/pmdartus/NanoCube/tarball/0.1",
     long_description=os.path.join(os.path.dirname(__file__), "Readme.md"),
     packages=find_packages(exclude=['test']),
     install_requires=reqs(),
