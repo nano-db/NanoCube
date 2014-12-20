3a8d3443b3cfb18d05beebceae8eb26384d85d95
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-12-20 09:32:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,16 +7,20 @@ def reqs():
     install_reqs = parse_requirements(req_path)
     return [str(ir.req) for ir in install_reqs]
 
+def readme():
+    with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+        return readme.read()
+
 setup(
     name="nanodb",
-    version="0.1",
+    version="0.1.1",
     license="Apache License 2.0",
     author="Pierre-Marie Dartus",
     author_email="dartus.pierremarie@gmail.com",
     url="https://github.com/pmdartus/NanoCube",
     description="In memory database for geolocated and temporal data",
-    download_url="https://github.com/pmdartus/NanoCube/tarball/0.1",
-    long_description=os.path.join(os.path.dirname(__file__), "Readme.md"),
+    download_url="https://github.com/pmdartus/NanoCube/tarball/0.1.1",
+    long_description=readme(),
     packages=find_packages(exclude=['test']),
     install_requires=reqs(),
     entry_points={
@@ -25,5 +29,12 @@ setup(
             "nanodb=libs.cli:init_parser"
         ]
     },
-    include_package_data=True
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Database :: Database Engines/Servers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2.7",
+    ]
 )
