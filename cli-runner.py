99171f1023eb4addc3cf9004fa4fe75fd377ff86
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-12-23 19:26:25 +0800

    Remove starting scripts

--- cli-runner.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from client.cli import init_parser
-
-
-if __name__ == '__main__':
-    init_parser()
--- server-runner.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from server.server import init_parser
-
-
-if __name__ == '__main__':
-    init_parser()
